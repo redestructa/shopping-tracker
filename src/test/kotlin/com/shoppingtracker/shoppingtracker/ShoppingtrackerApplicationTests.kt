6633da3ddf1b119f05ff9cda80956fa096e29940
Ralf Jäger <ralfjaeger85@gmail.com>
2021-10-28 21:17:44 +0200

    initial backend commit

---
+++ src/test/kotlin/com/shoppingtracker/shoppingtracker/ShoppingtrackerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.shoppingtracker.shoppingtracker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShoppingtrackerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

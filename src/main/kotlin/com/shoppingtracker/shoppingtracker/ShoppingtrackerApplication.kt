7dea5874b3f4ef5fb398edff836189e3191b5aba
Ralf Jäger <ralfjaeger85@gmail.com>
2021-10-28 21:32:04 +0200

    example rest api for buyings

--- src/main/kotlin/com/shoppingtracker/shoppingtracker/ShoppingtrackerApplication.kt
+++ src/main/kotlin/com/shoppingtracker/shoppingtracker/ShoppingtrackerApplication.kt
@@ -9,3 +9,14 @@ class ShoppingtrackerApplication
 fun main(args: Array<String>) {
 	runApplication<ShoppingtrackerApplication>(*args)
 }
+
+@RestController
+class MessageResource {
+	@GetMapping
+	fun index(): List<Message> = listOf(
+		Buying("example", 3.50),
+		Buying("another example", 30.99),
+	)
+}
+
+data class Buying(val description: String?, val price: Double)

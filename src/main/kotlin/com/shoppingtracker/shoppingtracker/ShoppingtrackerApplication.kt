62517c8e1a8a80342f65adc78e5c15ae9c2c3f7e
Ralf Jäger <ralfjaeger85@gmail.com>
2021-10-28 21:37:24 +0200

    corrected Dependencies / imports, run mvnw.cmd install  to get started

--- src/main/kotlin/com/shoppingtracker/shoppingtracker/ShoppingtrackerApplication.kt
+++ src/main/kotlin/com/shoppingtracker/shoppingtracker/ShoppingtrackerApplication.kt
@@ -1,7 +1,10 @@
 package com.shoppingtracker.shoppingtracker
 
+import org.springframework.web.bind.annotation.RestController
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.annotation.Id
+import org.springframework.web.bind.annotation.GetMapping
 
 @SpringBootApplication
 class ShoppingtrackerApplication
@@ -13,7 +16,7 @@ fun main(args: Array<String>) {
 @RestController
 class MessageResource {
 	@GetMapping
-	fun index(): List<Message> = listOf(
+	fun index(): List<Buying> = listOf(
 		Buying("example", 3.50),
 		Buying("another example", 30.99),
 	)
